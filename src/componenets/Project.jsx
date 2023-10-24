79c3eb1cbd527641f665c55b39882ecef47403ca
Khaizar Kanchwala <106642221+khaizarkanchwala@users.noreply.github.com>
2023-10-24 18:46:37 +0530

    Update Project.jsx

--- src/componenets/Project.jsx
+++ src/componenets/Project.jsx
@@ -45,7 +45,7 @@ const Project = () => {
   };
 
   return (
-    <div name="projects" className="bg-gradient-to-b  from-gray-800 to-black w-full text-white md:h-auto">
+    <div name="projects" className="bg-gradient-to-b  from-gray-800 to-black w-full py-20 text-white md:h-auto">
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">Projects</p>
